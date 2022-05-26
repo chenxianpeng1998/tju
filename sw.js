7548101ba95a6883178f617f492a173c4b7f1d84
陈宪鹏 <42299603+chenxianpeng1998@users.noreply.github.com>
2022-05-26 10:16:49 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,55 +1,29 @@
-'use strict'
-var cacheStorageKey = 'minimal-pwa-8'
-let cacheName = 'pwa-you-website-cache'; // 缓存名字
-
-var cacheList = [ // 所需缓存的文件
-  '/',
-  "index.html"
-]
-
-self.addEventListener('install', function(e) {
-  console.log('Cache event!')
-  e.waitUntil(
-    // 安装服务者时，对需要缓存的文件进行缓存
-    caches.open(cacheStorageKey).then(function(cache) {
-      console.log('Adding to Cache:', cacheList)
-      return cache.addAll(cacheList)
-    }).then(function() {
-      console.log('Skip waiting!')
-      return self.skipWaiting()
-    })
-  )
-})
-
-self.addEventListener('activate', function(e) {
-  // 判断地址是不是需要实时去请求，是就继续发送请求
-  console.log('Activate event')
-  e.waitUntil(
-    Promise.all(
-      caches.keys().then(cacheNames => {
-        return cacheNames.map(name => {
-          if (name !== cacheStorageKey) {
-            return caches.delete(name)
-          }
-        })
-      })
-    ).then(() => {
-      console.log('Clients claims.')
-      return self.clients.claim()
-    })
-  )
-})
-
-self.addEventListener('fetch', function(e) {
-  // 匹配到缓存资源，就从缓存中返回数据
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      if (response != null) {
-        console.log('Using cache for:', e.request.url)
-        return response
-      }
-      console.log('Fallback to fetch:', e.request.url)
-      return fetch(e.request.url)
-    })
-  )
-})
+//监听service worker事件
+self.addEventListener('install', function (event) {
+var homePage = new Request('index.html');
+event.waitUntil(
+//请求首页并将首页存入缓存
+fetch(homePage).then(function (response) {
+//建立cache-homePage缓存
+return caches.open('cache-homePage').then(function (cache) {
+return cache.put(homePage, response);
+});
+}));
+});
+//请求页面
+self.addEventListener('fetch', function (event) {
+event.respondWith(
+fetch(event.request).catch(function (error) {
+//请求失败，从缓存中读取缓存的页面
+return caches.open('cache-homePage').then(function (cache) {
+return cache.match('index.html');
+});
+}));
+});
+//刷新首页
+self.addEventListener('refreshHomePage', function (response) {
+return caches.open('cache-homePage').then(function (cache) {
+//将刷新后的页面缓存
+return cache.put(offlinePage, response);
+});
+});
